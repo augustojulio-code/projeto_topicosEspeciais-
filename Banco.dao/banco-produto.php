77f05ca21c141420a42d48debbe0ecb342f85f45
Julio <julio.mech@hotmail.com>
2022-10-13 15:57:47 -0300

    Teste List BD

--- Banco.dao/banco-produto.php
+++ Banco.dao/banco-produto.php
@@ -7,3 +7,23 @@ function inserir($conexao, $prod_nome, $prod_desc, $prod_preco, $prod_qtd)
 
     return mysqli_query($conexao, $sql);
 }
+
+function ListaProdutos($conexao)
+{
+    $produtos = array();
+
+    $result = mysqli_query($conexao, "select*from tb_produto");
+
+    while ($produto = mysqli_fetch_assoc($result)) {
+        array_push($produtos, $produto);
+    }
+
+    return $produtos;
+}
+
+function busca($conexao, $prod_id)
+{
+    $result = mysqli_query($conexao, "select*from tb_produto where tb_produto_id = $prod_id");
+
+    return mysqli_fetch_assoc($result);
+}
--- index.php
+++ index.php
@@ -1,22 +1,181 @@
 <?php
 
+$id = 'id_produtos';
+$nome = 'nome_produto';
+$desc = 'desc_produto';
+$preco = 'preco_produto';
+$qtd = 'qtd_produto';
 
-$paginas = ['Cadastro Produto' => '', 'sobre' => 'Pagina Sobre', 'contato' => ''];
+$paginas = ['Cadastro Produto' => '', 'Produtos' => 'Pagina Sobre', 'contato' => ''];
+
+$paginas['Produtos'] = '
+<?php
+    include ("../Banco/conexao.php");
+    include ("../Banco.dao/banco-produto.php");
+?>
+<style>
+.container {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+}
+</style>
+<div class="container">
+    <h1>Produtos Cadastrados</h1>
+    <table border="1">
+
+        <tr>
+            <td>Id</td>
+            <td>Nome</td>
+            <td>Descrição</td>
+            <td>Preço</td>
+            <td>QUantidade</td>
+        </tr>
+
+        <?php
+  $produtos = ListaProdutos($conexao);
+  foreach ($produtos as $produto):
+  ?>
+
+        <tr>
+
+            <td><?php echo $produto[$id]?></td>
+            <td><?php echo $produto[$nome]?></td>
+            <td><?php echo $produto[$desc]?></td>
+            <td><?php echo $produto[$preco]?></td>
+            <td><?php echo $produto[$qtd]?></td>
+            <td><a href="Excluir-Produto.php?tb_produto_id=
+        <?php echo $produto[$id]?>">
+                    Excluir</a></td>
+
+            <td><a href="Alterar-Produto.php?tb_produto_id=
+            <?php echo $produto[$id]?>">
+                    Alterar</a></td>
 
 
-$paginas['contato'] = '<form> <input type = "text" placeholder ="Nome" /> <button onclick ="imbecil()">clique aqui imbecil</button></form>';
+        </tr>
+
+        <?php
+endforeach;
+?>
+    </table>
+</div>';
+
+$paginas['contato'] = '<form> <input type="text" placeholder="Nome" /> <button onclick="imbecil()">clique aqui
+        imbecil</button></form>';
 
 $paginas['Cadastro Produto'] = '<br>
-<form method="post" action="Banco.dao/verificaCadastro.php">
-    <pre>
-        Nome      : <input type="text" name="txtprod_nome"><br>
-        Descrição : <textarea type="text" name="txtprod_desc"></textarea><br>
-        preço     : <input type="text" name="txtprod_preco"><br>
-        Quantidade: <input type="text" name="txtprod_qtd"><br>
-        
-        <input type="submit" value="Cadastrar" name="btn"><br>
-    </pre>
-</form>';
+<style>
+* {
+    box-sizing: border-box;
+}
+
+input[type=text],
+select,
+textarea {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    resize: vertical;
+}
+
+label {
+    padding: 12px 12px 12px 0;
+    display: inline-block;
+}
+
+input[type=submit] {
+    background-color: #04AA6D;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    float: right;
+}
+
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+
+.container {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+}
+
+.col-25 {
+    float: left;
+    width: 25%;
+    margin-top: 6px;
+}
+
+.col-75 {
+    float: left;
+    width: 75%;
+    margin-top: 6px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+    content: "";
+    display: table;
+    clear: both;
+}
+
+/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+
+    .col-25,
+    .col-75,
+    input[type=submit] {
+        width: 100%;
+        margin-top: 0;
+    }
+}
+</style>
+<div class="container">
+    <form method="post" action="Banco.dao/verificaCadastro.php">
+        <div class="row">
+            <div class="col-25">
+                <label for="fname">Produto</label>
+            </div>
+            <div class="col-75">
+                <input type="text" name="txtprod_nome" placeholder="Produto">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-25">
+                <label for="lname">Descrição</label>
+            </div>
+            <div class="col-75">
+                <input type="text" id="lname" name="txtprod_desc" placeholder="Descrição">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-25">
+                <label for="country">Preço</label>
+            </div>
+            <div class="col-75">
+                <input type="text" id="lname" name="txtprod_preco" placeholder="Preço">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-25">
+                <label for="subject">Quantidade</label>
+            </div>
+            <div class="col-75">
+                <input type="text" id="subject" name="txtprod_qtd" placeholder="Quantidade">
+                </intput>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <input type="submit" value="Submit">
+        </div>
+    </form>
+</div>';
 ?>
 
 <!DOCTYPE html>
